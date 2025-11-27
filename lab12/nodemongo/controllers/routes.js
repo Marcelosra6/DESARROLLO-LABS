4234e9ee51e14dbbcd10b7dd3e397088d1c8cae1
marcelosra6 <145044515+Marcelosra6@users.noreply.github.com>
2025-11-26 20:17:32 -0500

    Experiencia 2 del alb12

---
+++ lab12/nodemongo/controllers/routes.js
@@ -0,0 +1,36 @@
+// Importando paquetes y archivos necesarios para trabajar las rutas
+var express = require("express");
+var bodyParser = require("body-parser");
+var Product = require("../models/products");
+var router = express.Router();
+// Configurando el Parser
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+router.use(function (req, res, next) {
+  console.log("request");
+  next();
+});
+// Metodos para manejar Base de Datos mongoDB llamada node-crud
+router.route("/products")
+  .post(async function (req, res) {
+    try {
+      const product = new Product({
+        name: req.body.name,
+        amount: req.body.amount,
+        description: req.body.description,
+      });
+      await product.save();
+      res.json({ message: "Producto registrado con éxito" });
+    } catch (error) {
+      res.status(500).send("Error en el servicio: " + error);
+    }
+  })
+  .get(async function (req, res) {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).send("Error en el servicio: " + error);
+  }
+});
+module.exports = router;
